4af067653f5547cf4194d7d887ab885899298a65
Hepsibha <hepsiba17116@gmail.com>
2025-07-19 15:29:33 +0530

    Adding counting clicks state.jsx and state.css files

--- src/state.jsx
+++ src/state.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
  export default function State(){
-    const [IsImportant,setIsImportant] = React.useState("yes")
-     function HandleClick(){
-        setIsImportant("heck yes")
+    const [count,setCount]=React.useState(0)
+     function add(){
+        setCount(count+1)
+     }
+     function subtract(){
+        setCount(count-1)
      }
      return(
-         <main>
-             <h1 className="title">Is State Is Important?</h1>
-             <button onClick={HandleClick} className="button">{IsImportant}</button>
+         <main className="container">
+             <h1>
+                 Counting Clicks
+             </h1>
+             <div className="counter">
+                 <button className="minus" onClick={subtract} aria-label="decrease count">
+                     -
+                 </button>
+                 <h2 className="count">{count}</h2>
+                 <button className="plus" onClick={add} aria-label="increase count">
+                     +
+                 </button>
+             </div>
          </main>
-     )
+         )
  }
 
